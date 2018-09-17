3882338ec875753430d3c074721597339f694a64
jake park <jake@parkjake.com>
2018-09-17 09:09:40 -0400

    redux init

---
+++ reduxexample/src/components/Postform.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Postform extends Component {
+  constructor(props) {
+    this.state = {
+      posts: []
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>Add Post</h1>
+      </div>
+    )
+  }
+}
+
+export default Postform;
