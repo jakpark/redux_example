85c6279f57b52a608c1bc16016a66fb1cea0435a
jake park <jake@parkjake.com>
2018-09-17 21:07:27 -0400

    add PostForm

--- reduxexample/src/components/PostForm.js
+++ reduxexample/src/components/PostForm.js
@@ -9,6 +9,19 @@ class PostForm extends Component {
     return (
       <div>
         <h1>Add Post</h1>
+        <form>
+            <div>
+                <label>Title:</label><br></br>
+                <input type="text"/>
+            </div>
+            <div>
+                <label>Body:</label><br></br>
+                <textarea name="body">
+                </textarea>
+            </div>
+            <br/>
+            <button type="submit">Submit</button>
+        </form>
       </div>
     )
   }
