2452fd1a67aead71bf5251d8f23996853cf0503c
jake park <jake@parkjake.com>
2018-09-17 21:38:01 -0400

    bind onChange function to inputs

--- reduxexample/src/components/PostForm.js
+++ reduxexample/src/components/PostForm.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react'
 class PostForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+        title: '',
+        body: ''
+    };
+
+    this.onChange = this.onChange.bind(this);
   }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  
   render() {
     return (
       <div>
@@ -12,11 +22,11 @@ class PostForm extends Component {
         <form>
             <div>
                 <label>Title:</label><br></br>
-                <input type="text"/>
+                <input type="text" name="title" onChange={this.onChange} value={this.state.title}/>
             </div>
             <div>
                 <label>Body:</label><br></br>
-                <textarea name="body">
+                <textarea name="body" onChange={this.onChange} value={this.state.body}>
                 </textarea>
             </div>
             <br/>
